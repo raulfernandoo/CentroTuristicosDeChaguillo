e80e76ea8e9e7e014dcc4b6f08d8d4fec5473963
raulfernandoo <raulromero16.2021@gmail.com>
2021-08-28 18:34:28 -0500

    Guardar cambios bruscos

---
+++ index.js
@@ -0,0 +1,28 @@
+
+// const imagenDelTurismo = document.querySelector('.imagesDeTurismo')
+// const imagenDelTurismo2 = document.querySelector('.imagesDeTurismo2')
+// const imagenDelTurismo3= document.querySelector('.imagesDeTurismo3')
+// const imagenDelTurismo4 = document.querySelector('.imagesDeTurismo4')
+
+
+// const AddZ = (Propiedad) => {
+//     const bodyScrll = document.querySelector(".hola")
+//     const backroundImglolo = document.querySelector(".backround-img-turismo") 
+//     Propiedad.addEventListener("click" , () => {
+//         Propiedad.classList.add("Img-Saltada")
+//         bodyScrll.classList.add("body-scrolls")
+//         backroundImglolo.classList.add("backroundImglo")
+        
+        
+        
+
+
+
+// })
+// }
+
+
+// AddZ(imagenDelTurismo)
+// AddZ(imagenDelTurismo2)
+// AddZ(imagenDelTurismo3)
+// AddZ(imagenDelTurismo4)
